d404f6bbbe9cef27677c441698ea49e1cb499b12
w7147370218 <w7147370218@gmail.com>
2021-12-12 17:41:36 +0800

    commit with heroku

--- src/index.js
+++ src/index.js
@@ -18,17 +18,18 @@ const mongodbkey = key_json.mongodbkey;
 
 var mongoClient = require("mongodb").MongoClient;
 
-mongoClient.connect( mongodbkey, function (err, client) {
-    console.log("mongodb connect!");  
-    client.close();
-});
+
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+    console.log(Date());
+    console.log(new Date().toUTCString());
+    console.log(Date().toLocaleString());
+    console.log(Date().toLocaleString('zh-tw'));
     return res.sendFile(path.join(__dirname, '../public/client.html'));
 });
-
+    
 io.on("connection", (socket) => {
     console.log("server connect!!");
     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
